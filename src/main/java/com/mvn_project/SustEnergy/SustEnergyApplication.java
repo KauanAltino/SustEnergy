5fe562525f0ad1a0a0139960aa44a583a1b81380
KauanAltino <kauanaltino@gmail.com>
2024-11-11 14:33:11 -0300

    iniciando

---
+++ src/main/java/com/mvn_project/SustEnergy/SustEnergyApplication.java
@@ -0,0 +1,13 @@
+package com.mvn_project.SustEnergy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SustEnergyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SustEnergyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mvn_project/SustEnergy/SustEnergyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvn_project.SustEnergy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SustEnergyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
